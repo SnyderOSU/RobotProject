e787bc79bc4e3a1cc2a8ab54f1b019e61bfa5c72
SnyderOSU <159560906+SnyderOSU@users.noreply.github.com>
2024-03-18 10:57:04 -0400

    Add files via upload
    v2 failure

--- main.cpp
+++ main.cpp
@@ -19,16 +19,176 @@
 #include <string.h>
 #include <stdio.h>
 
-
-
-/* Define time for beep */
-//#define beep_t 10 // int milliseconds
+#define DriveWheelDiameter 1.65
+#define Pi 3.14
+#define degreeConvert 6
+#define blueval 1
+#define redval 0
 
 
 FEHMotor left_motor(FEHMotor::Motor0,9.0);
-FEHMotor right_motor(FEHMotor::Motor3,9.0);
-AnalogInputPin CdS_cell(FEHIO::P2_0);
+FEHMotor right_motor(FEHMotor::Motor2,9.0);
+AnalogInputPin CdS_cell(FEHIO::P2_3);
 
+// Declarations for analog optosensors
+AnalogInputPin right_opto(FEHIO::P2_0);
+AnalogInputPin middle_opto(FEHIO::P2_1);
+AnalogInputPin left_opto(FEHIO::P2_2);
+
+
+DigitalEncoder right_encoder(FEHIO::P0_1);
+DigitalEncoder left_encoder(FEHIO::P0_3);
+
+//positive percent is to the right
+void rotateAprox(int percent, int counts) //using encoders
+{
+    //Reset encoder counts
+    right_encoder.ResetCounts();
+    left_encoder.ResetCounts();
+
+    //Set both motors to desired percent
+    right_motor.SetPercent(percent);
+    left_motor.SetPercent(percent);
+
+    //While the average of the left and right encoder is less than counts,
+    //keep running motors
+    while((left_encoder.Counts()+right_encoder.Counts())/2<=counts)
+    {
+
+    }
+
+    //Turn off motors
+    right_motor.Stop();
+    left_motor.Stop();
+}
+
+void rotateDegree(int percent, int degree) //using encoders
+{
+    int counts=degreeConvert*degree;
+    LCD.WriteLine(counts);
+    //Reset encoder counts
+    right_encoder.ResetCounts();
+    left_encoder.ResetCounts();
+
+    //Set both motors to desired percent
+    right_motor.SetPercent(percent);
+    left_motor.SetPercent(percent);
+
+    //While the average of the left and right encoder is less than counts,
+    //keep running motors
+    while((left_encoder.Counts()+right_encoder.Counts())/2<=counts)
+    {
+    LCD.WriteLine(left_encoder.Counts());
+    LCD.WriteLine(right_encoder.Counts());
+    }
+    //Turn off motors
+    right_motor.Stop();
+    left_motor.Stop();
+}
+void rotateTime(int percent, float time) //using encoders
+{
+    //Reset encoder counts
+    right_encoder.ResetCounts();
+    left_encoder.ResetCounts();
+
+    //Set both motors to desired percent
+    right_motor.SetPercent(percent);
+    left_motor.SetPercent(percent);
+
+    Sleep(time);
+    //While the average of the left and right encoder is less than counts,
+    //keep running motors
+    //Turn off motors
+    right_motor.Stop();
+    left_motor.Stop();
+}
+
+void driveCounts(int percent, int counts) //using encoders
+{
+    //Reset encoder counts
+    right_encoder.ResetCounts();
+    left_encoder.ResetCounts();
+
+    //Set both motors to desired percent
+    right_motor.SetPercent(-percent);
+    left_motor.SetPercent(percent);
+
+    //While the average of the left and right encoder is less than counts,
+    //keep running motors
+    while((left_encoder.Counts()+right_encoder.Counts())/2<=counts);
+    LCD.WriteLine(left_encoder.Counts());
+    LCD.WriteLine(right_encoder.Counts());
+    //Turn off motors
+    right_motor.Stop();
+    left_motor.Stop();
+}
+void driveTime(int percent,float time)
+{
+    left_motor.SetPercent(percent);
+    right_motor.SetPercent(-percent);
+    Sleep(time);
+    left_motor.Stop();
+    right_motor.Stop();
+}
+void driveInches(int percent, float inches) //using encoders
+{
+    //Reset encoder counts
+    right_encoder.ResetCounts();
+    left_encoder.ResetCounts();
+
+    //Set both motors to desired percent
+
+    int counts = (inches/(DriveWheelDiameter*Pi))*318;
+    LCD.Clear();
+    LCD.Write("Driving Distance: ");
+    LCD.WriteLine(inches);
+    right_motor.SetPercent(-percent);
+    left_motor.SetPercent(percent);
+    //While the average of the left and right encoder is less than counts,
+    //keep running motors
+    while(((left_encoder.Counts()+right_encoder.Counts())/2)<=counts)
+    {
+    LCD.WriteLine(left_encoder.Counts());
+    LCD.WriteLine(right_encoder.Counts());
+    }
+    //Turn off motors
+    right_motor.Stop();
+    left_motor.Stop();
+}
+float moveWhileSensing(int percent,float inches)
+{
+    int counts = (inches/(DriveWheelDiameter*Pi))*318;
+    LCD.Clear();
+    int low = 4;
+    left_motor.SetPercent(percent);
+    right_motor.SetPercent(-percent);
+    while(((left_encoder.Counts()+right_encoder.Counts())/2)<=counts)
+    {
+        float val = CdS_cell.Value();
+        if (val<low)
+        {
+            low=val;
+            LCD.Write("Predicted For: ");
+            LCD.Write(low);
+            if(low<.7)
+            {
+                LCD.SetFontColor(RED);
+                LCD.WriteLine(": Red");
+                LCD.SetFontColor(BLACK);
+            }
+            else{
+                LCD.SetFontColor(BLUE);
+                LCD.WriteLine(": Blue");
+                LCD.SetFontColor(BLACK);
+            }
+        }
+        
+    }
+    left_motor.Stop();
+    right_motor.Stop();
+    return low;
+
+}
 void MoveDistance(float dist)
 {
     //motor ramping
@@ -179,31 +339,41 @@ void DriveBackTurnRight(double time,int ratio)
 /* Main function to control menu system */
 
 //1 sec is about 45 degrees
+
+//1150 is a 180 turn rights
 int main(void)
 {
+
+
+    left_encoder.ResetCounts();
+    right_encoder.ResetCounts();
+
     while (CdS_cell.Value()>2.75)
     {
         LCD.WriteLine(CdS_cell.Value());
     }
-    DriveForward(1.0);
-    RotateRight(2.0);
-    DriveForward(1.8);
-    RotateLeft(1.0);
-    Drive(1,10.0);
-    RotateLeft(2.0);
-    DriveForward(3.0);
-    RotateRight(2.0);
-    DriveForward(3.0);
+    LCD.WriteLine("Starting");
+    rotateDegree(50,45);
+    driveInches(50,40);
+    driveInches(-50,5);
+    rotateDegree(-50,90);
+    driveInches(50,12);
+    rotateDegree(50,90);
+    
 
-    DriveBackward(5.0);
-    DriveForward(1.0);
-    RotateRight(2.0);
-    DriveForward(4.0);
-    DriveBackward(0.2);
-    RotateRight(2.0);
-    DriveForward(10.0);
+    // do cds check
+    float sensor=moveWhileSensing(50,20);
+    if(sensor <.7)
+    {
+        LCD.SetFontColor(RED);
+        LCD.WriteLine("Running Red");
+        LCD.SetFontColor(BLACK);
+        //run red
+    }
 
-    //RotateLeft(5.0);
-   // RotateRight(5.0);
+
+    //Initialize the screen
+
+
 }
 
